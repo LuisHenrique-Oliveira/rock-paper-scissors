aa53f44b45f173e480a6c3886ed07edda8942390
Luís Henrique de Oliveira <luishenriquedeoliveirabr@gmail.com>
2025-01-25 09:02:42 -0300

    Create new function getComputerChoice
    To randomly return one of the strings:
    "Rock", "Paper" or "Scissors"
    
    Criar nova função getComputerChoice
    
    Para aleatoriamente retornar uma das strings:
    "Pedra", "Papel" ou "Tesoura"

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Hello World");
+//Create a function getComputerChoice
+//Create a variable for choice that will be equal to a random number between one and three
+//if choice is equal to 1
+// choice will have a new string value of Rock
+// return choice
+//Else if choice is equal to 2
+// choice will have a new string value of Paper
+// return choice
+//Else if choice is equal to 3
+// choice will have a new string value of Scissors
+// return choice
+
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() *  3) + 1;
+    if (choice === 1){
+        choice = "Rock";
+        console.log(choice)
+    }
+    else if(choice === 2){
+        choice = "Paper";
+        console.log(choice)
+    }
+    else if(choice === 3){
+        choice = "Scissors";
+        console.log(choice)
+    }
+    else {
+        console.log("putz")
+    }
+}
+
+getComputerChoice();
