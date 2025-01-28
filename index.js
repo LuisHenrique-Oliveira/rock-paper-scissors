ea10de6aa6712574b830071ac0f6d13180644905
Luís Henrique de Oliveira <luishenriquedeoliveirabr@gmail.com>
2025-01-28 07:32:40 -0300

    Update the playRound function
    To keep track of the score
    
    Atualizar a função playRound
    
    Para acompanhar a pontuação

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
 
-
-
-
 //Create a function getComputerChoice
 //Create a variable for choice that will be equal to a random number between one and three
 //if choice is equal to 1
@@ -15,7 +12,6 @@ let computerScore = 0;
 //Else if choice is equal to 3
 // choice will have a new string value of Scissors
 // return choice
-
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() *  3) + 1;
@@ -37,116 +33,60 @@ function getComputerChoice() {
         
     }
     else {
-        console.log("putz")
+        console.log("putz");
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
 //Create a function getHumanChoice
+//Create a variable for Choice and ask for the user "Choose Rock, Paper or Scissors" and put answer in choice
+//Turn the choice to lower case so it can be case insensitive 
+//if choice is equal to Rock
+// return choice
+//if choice is equal to Paper
+// return choice
+//if choice is equal to Scissors
+// // return choice
+
 function getHumanChoice(){
-    //Create a variable for Choice and ask for the user "Choose Rock, Paper or Scissors" and put answer in choice
+    
     let humanChoice = prompt("Chosse between Rock, Paper or Scissors");
-    //Turn the choice to lower case so it can be case insensitive 
+   
     humanChoice = humanChoice.toLocaleLowerCase();
 
     if(!humanChoice){
         alert("Por favor, insira uma escolha válida!");
     }
-    //if choice is equal to Rock
+   
     else if (humanChoice == "rock" || humanChoice == "pedra"){
         humanChoice = "Rock";
-        // return choice
+       
         console.log(humanChoice);
         return humanChoice;
     }
-    //if choice is equal to Paper
+    
     else if(humanChoice == "paper" || humanChoice == "papel"){
         humanChoice = "Paper";
-        // return choice
+        
         console.log(humanChoice);
         return humanChoice;
     }
-    //if choice is equal to Scissors
+    
     else if(humanChoice == "scissors" || humanChoice == "tesoura"){
         humanChoice = "Scissors";
-        // return choice
+        
         console.log(humanChoice);
         return humanChoice;
     }
       
     else{
-        console.log("Por favor,insira uma escolha válida!")
+        console.log("Por favor,insira uma escolha válida!");
     }
 }
-
-
-
-
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
 
 //Create new function playRound
 //Take humanChoice and computerChoice as parameters 
-function playRound(humanChoice, computerChoice){
-    //If humanChoice is equal to Rock and computerChoice is equal to Scissors
-    if(humanChoice === "Rock" && computerChoice === "Scissors"){
-        //Return "You win! Rock beats Scissors"
-        console.log("You win! Rock beats Scissors");
-        humanScore = 1;
-        return humanScore;
-    }
-    else if(humanChoice === "Scissors" && computerChoice === "Paper"){
-        console.log("You win! Scissors beats Paper");
-        humanScore = 1;
-        return humanScore;
-    }
-    else if(humanChoice === "Paper" && computerChoice === "Rock"){
-        console.log("You win! Paper beats Rock");
-        humanScore = 1;
-        return humanScore;
-    }
-    //situações de perca
-    else if(humanChoice === "Rock" && computerChoice === "Paper"){
-        console.log("You lose! Womp womp, paper beats Rock");
-        computerScore = 1;
-        return computerScore;
-    }
-    else if(humanChoice === "Paper" && computerChoice === "Scissors"){
-        console.log("You lose! Womp womp, Scissors beats Paper");
-        computerScore = 1;
-        return computerScore;
-    }
-    else if(humanChoice === "Scissors" && computerChoice === "Rock"){
-        console.log("You lose! Womp womp, Rock beats Scissors");
-        computerScore = 1;
-        return computerScore;
-    }
-    else if(humanChoice === computerChoice ){
-        humanScore = 1;
-        computerScore = 1;
-        console.log("Tie!");
-    }
-    else{
-        console.log("putz")
-    }
-}
-
-playRound(humanChoice, computerChoice);
-console.log(humanScore, computerScore)
-
-
-
+//If humanChoice is equal to Rock and computerChoice is equal to Scissors
+//Return "You win! Rock beats Scissors"
 //Else If humanChoice is equal to Scissors and computerChoice is equal to Paper
 //Return "You win! Scissors beats Paper"
 //Else If humanChoice is equal to Paper and computerChoice is equal to Rock
@@ -157,4 +97,51 @@ console.log(humanScore, computerScore)
 //Else If humanChoice is equal to Paper and computerChoice is equal to Scissors
 //Return "You lose! Scissors beats Paper"
 //Else If humanChoice is equal to Rock and computerChoice is equal to Paper
-//Return "You lose! Paper beats Rock
+//Return "You lose! Paper beats Rock"
+
+function playRound(humanChoice, computerChoice){
+  
+    if(humanChoice === "Rock" && computerChoice === "Scissors"){
+        
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+       
+    }
+    else if(humanChoice === "Scissors" && computerChoice === "Paper"){
+        console.log("You win! Scissors beats Paper");
+        humanScore++;
+ 
+    }
+    else if(humanChoice === "Paper" && computerChoice === "Rock"){
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+       
+    }
+    //situações de perca
+    else if(humanChoice === "Rock" && computerChoice === "Paper"){
+        console.log("You lose! Womp womp, paper beats Rock");
+        computerScore++;
+        
+    }
+    else if(humanChoice === "Paper" && computerChoice === "Scissors"){
+        console.log("You lose! Womp womp, Scissors beats Paper");
+        computerScore++;
+       
+    }
+    else if(humanChoice === "Scissors" && computerChoice === "Rock"){
+        console.log("You lose! Womp womp, Rock beats Scissors");
+        computerScore++;
+        
+    }
+    else if(humanChoice === computerChoice ){
+        console.log("Tie!");
+    }
+    else{
+        console.log("putz");
+    }
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
+console.log(humanScore, computerScore);
